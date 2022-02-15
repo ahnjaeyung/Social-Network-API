960ff743219ac4c850d1f27a6f8591ac1a46eca3
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-02-14 20:57:51 -0600

    added routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  createThought,
+  getSingleThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(removeThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+
+module.exports = router;
